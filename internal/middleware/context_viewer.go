e5f5cc0d6bc863feb7d901bb1ea127aad5679dd6
Loc Nguyen <lochnguyen@gmail.com>
2024-04-30 20:10:55 -0700

    Lint fix

--- internal/middleware/context_viewer.go
+++ internal/middleware/context_viewer.go
@@ -12,7 +12,7 @@ func AddContextViewer(appCtx *app.AppContext, next http.Handler) http.HandlerFun
 	return func(w http.ResponseWriter, r *http.Request) {
 		if appCtx.SessionManager.Exists(r.Context(), ent.ContextViewerKey.Name) {
 			u := appCtx.SessionManager.Get(r.Context(), ent.ContextViewerKey.Name).(*ent.User)
-			r = r.WithContext(context.WithValue(r.Context(), ent.ContextViewerKey.Name, u))
+			r = r.WithContext(context.WithValue(r.Context(), ent.ContextViewerKey, u))
 			next.ServeHTTP(w, r)
 		} else {
 			next.ServeHTTP(w, r)
